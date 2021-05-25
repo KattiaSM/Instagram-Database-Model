0aa3cfb1f412e073300dad471bfa61375b8590df
Kattia Salazar Mesen <kattiasm@gmail.com>
2021-05-25 16:04:29 +0000

    Entregable Instagram con diagrama realizado

--- src/models.py
+++ src/models.py
@@ -1,7 +1,6 @@
 import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy_imageattach.entity import Image, image_attachment
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -11,65 +10,35 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(30), nullable=False)
-    username = Column(String(30), nullable=False)
+    username = Column(String(50), nullable=False)
+    firstname = Column(String(50), nullable=False)
+    lastname = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False)
-    password = Column(String(10), nullable=False)
-    profile_photo = image_attachment (profile_photo)
-    biography = Column(String(150), nullable=False)
-    location = Column(String(50), nullable=False)
-    highlights = Column(String(20), nullable=False)
-
-class Profile_Photo(Base, Image):
-    __tablename__ = profile_photo
-    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-    user = relationship(User)
 
 class Post(Base):
     __tablename__ = 'post'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-    photo = image_attachment (post_photo)
-    description = Column(String(250))
 
-class Post_Photo(Base, Image):
-    __tablename__ = post_photo
-    user_id = Column(Integer, ForeignKey('post.id'), primary_key=True)
-    post = relationship(Post)
-
-class Comments(Base):
-    __tablename__ = 'comments'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+    
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(1000), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'))
     post_id = Column(Integer, ForeignKey('post.id'))
-    post = relationship(Post)
-    user_id = Column(String(30))
-    content = Column(String(250))
-
-class Like(Base):
-    __tablename__ = 'likes'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    
+class Media(Base):
+    __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('post.id'))
-    post = relationship(Post)
-    post_id = Column(String(50))
-
-class Followers(Base):
-    __tablename__ = 'followers'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-    followers_id = Column(String(50))
+    url = Column(String(1000))
+    post_id = Column(Integer, ForeignKey('post.id'))
 
     def to_dict(self):
         return {}
